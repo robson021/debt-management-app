e18323e5a60ffb119821c3acd55c05d876f0e030
robert <nowak021@gmail.com>
2017-04-29 17:46:04 +0200

    fix styles

--- frontend/src/app/app.router.ts
+++ frontend/src/app/app.router.ts
@@ -5,6 +5,7 @@ import {LoginComponent} from './login/login.component';
 export const router: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
+    {path: 'register', component: LoginComponent},
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
