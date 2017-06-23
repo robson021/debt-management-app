57174bbeee824936a2b06a334747faf4344d3004
robert <nowak021@gmail.com>
2017-06-23 23:49:26 +0200

    fix dev settings

--- backend/src/main/java/robert/db/DevSettings.java
+++ backend/src/main/java/robert/db/DevSettings.java
@@ -1,19 +1,19 @@
 package robert.db;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-
-import javax.annotation.PostConstruct;
-
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-
 import robert.db.entities.User;
 import robert.db.svc.api.PaymentService;
 import robert.db.svc.api.UserService;
 import robert.web.rest.dto.PaymentDTO;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Profile("dev")
 @Component
@@ -37,7 +37,8 @@ public class DevSettings {
 		user.setSurname("User");
 		user.setPassword("Passwd.123");
 
-		List<User> users = Arrays.asList(new User(), new User(), new User(), new User());
+		List<User> users = Stream.of(new User(), new User(), new User(), new User())
+				.collect(Collectors.toList());
 
 		users.forEach(u -> {
 			u.setEmail("user@mail." + RandomStringUtils.randomAlphabetic(3)
