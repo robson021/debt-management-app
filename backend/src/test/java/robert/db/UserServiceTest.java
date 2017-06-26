d09de29a74da9beba8c7ba54eff32431e839712a
robert <nowak021@gmail.com>
2017-06-26 22:38:07 +0200

    add user service test

--- backend/src/test/java/robert/db/UserServiceTest.java
+++ backend/src/test/java/robert/db/UserServiceTest.java
@@ -3,6 +3,7 @@ package robert.db;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,18 +11,48 @@ import robert.db.entities.User;
 import robert.db.svc.api.UserService;
 import robert.tools.SpringTest;
 import robert.tools.TestUtils;
+import robert.web.rest.dto.UserInfoDTO;
 
 public class UserServiceTest extends SpringTest {
 
 	@Autowired
 	private UserService userService;
 
+	private User user;
+
+	@Before
+	public void setup() {
+		user = userService.saveNewUser(TestUtils.generateNewUser());
+	}
+
 	@Test
 	public void findOtherUsersExceptGiven() {
-		User user = userService.saveNewUser(TestUtils.generateNewUser());
 		List<User> otherUsers = userService.findOtherUsersExceptGiven(user.getId());
 
 		Assertions.assertThat(otherUsers)
 				.doesNotContain(user);
 	}
+
+	@Test
+	public void findUserByEmail() {
+		User userByEmail = userService.findUserByEmail(user.getEmail());
+		Assertions.assertThat(userByEmail)
+				.isEqualTo(user);
+	}
+
+	@Test
+	public void saveNewDtoUser() {
+		UserInfoDTO userInfoDTO = new UserInfoDTO();
+		userInfoDTO.setAccountNo("12342422");
+		userInfoDTO.setEmail("dto.test@aa.com");
+		userInfoDTO.setName("aaaaaa");
+		userInfoDTO.setSurname("bbbb");
+		userInfoDTO.setPassword("Passs.123");
+
+		User user = userService.saveNewUser(userInfoDTO);
+
+		Assertions.assertThat(user.getId())
+				.isGreaterThan(0);
+	}
+
 }
