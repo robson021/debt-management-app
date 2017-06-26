619a41bdfb458d997ca3a37e0b657a35cfb124e0
rnowak <robert.nowak2@orangeretail.pl>
2017-06-26 16:17:04 +0200

    refactor PaymentService tests

--- backend/src/test/java/robert/db/PaymentServiceTest.java
+++ backend/src/test/java/robert/db/PaymentServiceTest.java
@@ -10,78 +10,103 @@ import robert.db.svc.api.PaymentService;
 import robert.db.svc.api.UserService;
 import robert.tools.SpringTest;
 import robert.tools.TestUtils;
-import robert.web.rest.dto.PaymentDTO;
 
 import java.util.List;
 
 public class PaymentServiceTest extends SpringTest {
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@Autowired
-	private PaymentService paymentService;
+    @Autowired
+    private PaymentService paymentService;
 
-	private User borrower;
+    private User borrower;
 
-	private User lender;
+    private User lender;
 
-	@Before
-	public void setup() {
-		borrower = userService.saveNewUser(TestUtils.generateNewUser());
-		lender = userService.saveNewUser(TestUtils.generateNewUser());
-	}
+    @Before
+    public void setup() {
+        borrower = userService.saveNewUser(TestUtils.generateNewUser());
+        lender = userService.saveNewUser(TestUtils.generateNewUser());
 
-	@Test
-	public void cancelDebt() throws Exception {
-		paymentService.addDebtor(lender.getId(), TestUtils.generatePayment(borrower));
+        paymentService.addDebtor(lender.getId(), TestUtils.generatePayment(borrower));
+        paymentService.addDebtor(lender.getId(), TestUtils.generatePayment(borrower));
+    }
 
-		List<Asset> assets = paymentService.findUserDebtors(lender.getId());
-		Assertions.assertThat(assets)
-				.hasSize(1);
+    @Test
+    public void findUserDebts() {
+        List<Asset> userDebts = paymentService.findUserDebts(borrower.getId());
 
-		long assetId = assets.iterator()
-				.next()
-				.getId();
+        Assertions.assertThat(userDebts.size())
+                .isEqualTo(2);
 
-		paymentService.cancelDebt(assetId, lender.getId());
+        Assertions.assertThat(userDebts.get(0).getAmount())
+                .isGreaterThan(.0);
 
-		// after delete
-		assets = paymentService.findUserDebtors(lender.getId());
-		Assertions.assertThat(assets)
-				.hasSize(0);
-	}
+        for (Asset userDebt : userDebts) {
+            Assertions.assertThat(userDebt.getAmount())
+                    .isGreaterThan(.0);
+        }
+    }
 
-	@Test
-	public void addDebtor() throws Exception {
-		paymentService.addDebtor(lender.getId(), TestUtils.generatePayment(borrower));
-		paymentService.addDebtor(lender.getId(), TestUtils.generatePayment(borrower));
+    @Test
+    public void findUserDebtors() {
+        List<Asset> userDebtors = paymentService.findUserDebtors(lender.getId());
 
-		List<Asset> debtors = paymentService.findUserDebtors(lender.getId());
+        Assertions.assertThat(userDebtors.size())
+                .isEqualTo(2);
 
-		Assertions.assertThat(debtors)
-				.hasSize(2);
-	}
+        for (Asset debtor : userDebtors) {
+            double amount = debtor.getAmount();
+            Assertions.assertThat(amount)
+                    .isGreaterThan(.0);
+        }
+    }
 
-	@Test
-	public void getBalance() {
-		paymentService.addDebtor(lender.getId(), TestUtils.generatePayment(borrower));
-		paymentService.addDebtor(lender.getId(), TestUtils.generatePayment(borrower));
+    @Test
+    public void cancelDebt() throws Exception {
+        List<Asset> assets = paymentService.findUserDebtors(lender.getId());
 
-		double balance = paymentService.getUserDebtBalance(lender.getId());
-		Assertions.assertThat(balance)
-				.isGreaterThan(0);
-	}
+        final int initialAssetsSize = assets.size();
 
-	@Test
-	public void getBalanceBetweenUsers() {
-		PaymentDTO paymentDTO = TestUtils.generatePayment(borrower);
-		paymentDTO.setAmount(25.22);
-		paymentService.addDebtor(lender.getId(), paymentDTO);
+        Assertions.assertThat(initialAssetsSize)
+                .isGreaterThan(0);
 
-		double debt = paymentService.getMoneyBalanceWithOtherUser(lender.getId(), borrower.getId());
-		Assertions.assertThat(debt)
-				.isEqualTo(paymentDTO.getAmount());
-	}
+        long assetId = assets.iterator()
+                .next()
+                .getId();
+
+        paymentService.cancelDebt(assetId, lender.getId());
+
+        // after delete
+        assets = paymentService.findUserDebtors(lender.getId());
+        Assertions.assertThat(assets)
+                .hasSize(initialAssetsSize - 1);
+    }
+
+    @Test
+    public void addDebtor() throws Exception {
+        List<Asset> debtors = paymentService.findUserDebtors(lender.getId());
+
+        Assertions.assertThat(debtors)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getBalance() {
+        double balance = paymentService.getUserDebtBalance(lender.getId());
+        Assertions.assertThat(balance)
+                .isGreaterThan(0);
+    }
+
+    @Test
+    public void getBalanceBetweenUsers() {
+        paymentService.addDebtor(lender.getId(), TestUtils.generatePayment(borrower));
+        double debt = paymentService.getMoneyBalanceWithOtherUser(lender.getId(), borrower.getId());
+
+        Assertions.assertThat(debt)
+                .isGreaterThan(.0);
+    }
 
 }
