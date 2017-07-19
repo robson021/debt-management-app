b51662a5532292fe159ded3fa5ab63f734254b18
robert <nowak021@gmail.com>
2017-07-19 21:32:22 +0200

    add actuator, remove cache

--- backend/src/main/java/robert/DebtManagementApplication.java
+++ backend/src/main/java/robert/DebtManagementApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -14,7 +13,6 @@ import robert.web.security.auth.JwtUtils;
 @EntityScan(basePackages = "robert.db.entities")
 @EnableJpaRepositories
 @EnableTransactionManagement
-@EnableCaching
 public class DebtManagementApplication {
 
 	public static void main(String[] args) {
