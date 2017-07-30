6ed26b1add71ac290b52dba26299c0e1ca957020
robert <nowak021@gmail.com>
2017-07-30 23:13:15 +0200

    refactor auth exception

--- backend/src/main/java/robert/exeptions/AuthException.java
+++ backend/src/main/java/robert/exeptions/AuthException.java
@@ -3,7 +3,7 @@ package robert.exeptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "Authentication failed")
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Authentication failed")
 public class AuthException extends RuntimeException {
 
 	public AuthException(String msg) {
