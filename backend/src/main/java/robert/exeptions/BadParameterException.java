a062d0436ee5097c7988da75e6f49e6c14000759
robert <nowak021@gmail.com>
2017-08-05 00:57:20 +0200

    refactor exceptions

--- backend/src/main/java/robert/exeptions/BadParameterException.java
+++ backend/src/main/java/robert/exeptions/BadParameterException.java
@@ -1,5 +1,9 @@
 package robert.exeptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Not allowed parameter")
 public class BadParameterException extends RuntimeException {
 
 	public BadParameterException(String message) {
--- backend/src/main/java/robert/exeptions/InvalidJwtException.java
+++
@@ -1,9 +0,0 @@
-package robert.exeptions;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class InvalidJwtException extends AuthenticationException {
-	public InvalidJwtException(String msg) {
-		super(msg);
-	}
-}
