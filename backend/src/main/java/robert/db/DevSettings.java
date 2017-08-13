261295d1766e680483b7eefd526d37da0106ae23
robert <nowak021@gmail.com>
2017-08-13 13:25:57 +0200

    refactor dev settings. update logging config.

--- backend/src/main/java/robert/db/DevSettings.java
+++ backend/src/main/java/robert/db/DevSettings.java
@@ -9,9 +9,8 @@ import robert.db.svc.api.UserService;
 import robert.web.rest.dto.PaymentDTO;
 
 import javax.annotation.PostConstruct;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
+import java.util.stream.Stream;
 
 @Profile("dev")
 @Component
@@ -27,7 +26,7 @@ public class DevSettings {
 	}
 
 	@PostConstruct
-	public void init() throws Exception {
+	void init() throws Exception {
 		String testUserEmail = "test@t.pl";
 		User user = new User();
 		user.setEmail(testUserEmail);
@@ -37,18 +36,17 @@ public class DevSettings {
 		user.setAdminRole(true);
 		user.setAccountNo(RandomStringUtils.randomNumeric(10));
 
-		List<User> users = Arrays.asList(new User(), new User(), new User(), new User());
-
-		users.forEach(u -> {
-			u.setEmail("user@mail." + RandomStringUtils.randomAlphabetic(3).toLowerCase());
-			u.setName(RandomStringUtils.randomAlphabetic(6));
-			u.setSurname(RandomStringUtils.randomAlphabetic(6));
-			String password = "P.1" + RandomStringUtils.randomAlphanumeric(7);
-			u.setPassword(password);
-			u.setAccountNo(RandomStringUtils.randomNumeric(10));
-			u.setAccountNo(RandomStringUtils.randomNumeric(12));
-			userService.saveNewUser(u);
-		});
+		Stream.of(new User(), new User(), new User(), new User())
+				.forEach(u -> {
+					u.setEmail("user@mail." + RandomStringUtils.randomAlphabetic(3).toLowerCase());
+					u.setName(RandomStringUtils.randomAlphabetic(6));
+					u.setSurname(RandomStringUtils.randomAlphabetic(6));
+					String password = "P.1" + RandomStringUtils.randomAlphanumeric(7);
+					u.setPassword(password);
+					u.setAccountNo(RandomStringUtils.randomNumeric(10));
+					u.setAccountNo(RandomStringUtils.randomNumeric(12));
+					userService.saveNewUser(u);
+				});
 
 		userService.saveNewUser(user);
 		System.out.println("saved test user: " + user.toString());
@@ -56,15 +54,14 @@ public class DevSettings {
 		long userId = userService.findUserByEmail(testUserEmail).getId();
 		User borrower = userService.findUserById(userId - 1);
 
-		List<PaymentDTO> payments = Arrays.asList(new PaymentDTO(), new PaymentDTO());
-
-		payments.forEach(p -> {
-			p.setAmount(new Random().nextDouble() * 150);
-			p.setDescription("Example Paymnet");
-			p.setBorrowerSurname(borrower.getSurname());
-			p.setBorrowerName(borrower.getName());
-			p.setBorrowerId(borrower.getId());
-			paymentService.addDebtor(userId, p);
-		});
+		Stream.of(new PaymentDTO(), new PaymentDTO())
+				.forEach(p -> {
+					p.setAmount(new Random().nextDouble() * 150);
+					p.setDescription("Example Paymnet");
+					p.setBorrowerSurname(borrower.getSurname());
+					p.setBorrowerName(borrower.getName());
+					p.setBorrowerId(borrower.getId());
+					paymentService.addDebtor(userId, p);
+				});
 	}
 }
