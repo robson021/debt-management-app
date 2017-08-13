2f1679098d5ddd73304656e35868f416513c0785
robert <nowak021@gmail.com>
2017-08-13 14:28:35 +0200

    add SSL

---
+++ backend/src/main/java/robert/web/security/SslConfig.java
@@ -0,0 +1,16 @@
+package robert.web.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Profile("prod")
+@PropertySource("classpath:ssl.properties")
+public class SslConfig {
+}
+/*
+* generate key with:
+* "keytool -genkey -alias serverSSL -storetype PKCS12 -keyalg RSA -keysize 2048 -keystore keystore.p12 -validity 3650"
+* than put it into src/resources and set values in ssl.properties (alias & password)
+*/
