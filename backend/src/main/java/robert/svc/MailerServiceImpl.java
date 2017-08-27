1980ce671431309c0e07e22fae310c5ec1e729e3
robert <nowak021@gmail.com>
2017-08-27 13:29:21 +0200

    refactor mailer service

--- backend/src/main/java/robert/svc/MailerServiceImpl.java
+++ backend/src/main/java/robert/svc/MailerServiceImpl.java
@@ -22,49 +22,51 @@ import robert.svc.api.MailerService;
 @Profile("prod")
 public class MailerServiceImpl implements MailerService {
 
-	private static final Logger log = LoggerFactory.getLogger(MailerServiceImpl.class);
+    private static final Logger log = LoggerFactory.getLogger(MailerServiceImpl.class);
 
-	private final String applicationLogFile;
+    private final String applicationLogFile;
 
-	private final JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
 
-	private final AsyncTaskService asyncTaskService;
+    private final AsyncTaskService asyncTaskService;
 
-	public MailerServiceImpl(@Value("${logging.file}") String applicationLogFile, JavaMailSender mailSender, AsyncTaskService asyncTaskService) {
-		this.applicationLogFile = applicationLogFile;
-		this.mailSender = mailSender;
-		this.asyncTaskService = asyncTaskService;
-		log.info("Application log file for mail service: '{}'", this.applicationLogFile);
-	}
+    public MailerServiceImpl(@Value("${logging.file}") String applicationLogFile, JavaMailSender mailSender, AsyncTaskService asyncTaskService) {
+        this.applicationLogFile = applicationLogFile;
+        this.mailSender = mailSender;
+        this.asyncTaskService = asyncTaskService;
+        log.info("Application log file for mail service: '{}'", this.applicationLogFile);
+    }
 
-	@Override
-	public void sendServerLogs(String receiverEmail) {
-		log.info("Server logs request to {}", receiverEmail);
-		sendEmail(receiverEmail, "Server log", "Debts management app - " + new Date(), new File(applicationLogFile), false);
-	}
+    @Override
+    public void sendServerLogs(String receiverEmail) {
+        log.info("Server logs request to {}", receiverEmail);
+        sendEmail(receiverEmail, "Server log", "Debts management app - " + new Date(), new File(applicationLogFile), false);
+    }
 
-	@Override
-	public void sendEmail(String receiverEmail, String topic, String body, File file, boolean deleteFileAfterIsSent) {
-		Assert.hasText(receiverEmail, "Receiver cannot be null");
-		asyncTaskService.submit(() -> {
-			try {
-				MimeMessage mimeMessage = mailSender.createMimeMessage();
-				MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-				helper.setTo(receiverEmail);
-				helper.setSubject(topic);
-				helper.setText(body);
-				if ( file != null ) {
-					FileSystemResource attachment = new FileSystemResource(file);
-					helper.addAttachment(attachment.getFilename(), attachment);
-				}
-			} catch (Exception ignored) {
-				log.error("Could not send server logs to {}", receiverEmail);
-			} finally {
-				if ( deleteFileAfterIsSent && file != null ) {
-					FileSystemUtils.deleteRecursively(file);
-				}
-			}
-		});
-	}
+    @Override
+    public void sendEmail(String receiverEmail, String topic, String body, File file, boolean deleteFileAfterIsSent) {
+        Assert.hasText(receiverEmail, "Receiver cannot be null");
+        asyncTaskService.submit(() -> {
+            try {
+                MimeMessage mimeMessage = mailSender.createMimeMessage();
+                MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+                helper.setTo(receiverEmail);
+                helper.setSubject(topic);
+                helper.setText(body);
+                if ( file != null ) {
+                    FileSystemResource attachment = new FileSystemResource(file);
+                    helper.addAttachment(attachment.getFilename(), attachment);
+                }
+                mailSender.send(mimeMessage);
+                log.info("Mail has been sent to {}", receiverEmail);
+            } catch (Exception ex) {
+                log.error("Could not send server logs to {}\n{}", receiverEmail, ex.getMessage());
+            } finally {
+                if ( deleteFileAfterIsSent && file != null ) {
+                    FileSystemUtils.deleteRecursively(file);
+                }
+            }
+        });
+    }
 
 }
