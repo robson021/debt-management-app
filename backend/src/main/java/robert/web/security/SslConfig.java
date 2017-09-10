44bc971e9f1a4bab27ac14d72d9bded1995c3e4e
robert <nowak021@gmail.com>
2017-09-10 23:32:46 +0200

    update ssl

--- backend/src/main/java/robert/web/security/SslConfig.java
+++ backend/src/main/java/robert/web/security/SslConfig.java
@@ -9,8 +9,3 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("classpath:ssl.properties")
 public class SslConfig {
 }
-/*
-* generate key with:
-* "keytool -genkey -alias serverSSL -storetype PKCS12 -keyalg RSA -keysize 2048 -keystore keystore.p12 -validity 3650"
-* than put it into src/resources and set values in ssl.properties (alias & password)
-*/
