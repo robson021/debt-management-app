82b7fc00c1b635d287a26e52a81b7873da9448ba
robert <nowak021@gmail.com>
2017-12-11 22:33:54 +0100

    simplify unit tests

--- backend/src/test/java/robert/tools/SpringWebMvcTest.java
+++ backend/src/test/java/robert/tools/SpringWebMvcTest.java
@@ -1,22 +1,13 @@
 package robert.tools;
 
-import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
+@AutoConfigureMockMvc(addFilters = false)
 public abstract class SpringWebMvcTest extends SpringTest {
 
     @Autowired
-    private WebApplicationContext wac;
-
     protected MockMvc mockMvc;
-
-    @Before
-    public void init() throws Exception {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac)
-                .build();
-    }
 
 }
