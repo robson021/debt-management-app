2fdc0aca6e005e794dbb60cd02d522962463ef02
robert <nowak021@gmail.com>
2018-04-04 00:15:55 +0200

    add basic auth

--- backend/src/main/java/robert/DebtManagementApplication.java
+++ backend/src/main/java/robert/DebtManagementApplication.java
@@ -1,6 +1,5 @@
 package robert;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -11,10 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import robert.svc.api.MailerService;
 
@@ -34,20 +29,9 @@ public class DebtManagementApplication {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(11);
-    }
-
-    @Bean
     @Lazy
     @ConditionalOnMissingBean
     public MailerService mailerService() {
         return (receiverEmail, topic, body, file, deleteFileAfterIsSent) -> System.out.println("[mock] sending email to: " + receiverEmail);
-    }
-
-    @Autowired
-    public void authenticationManager(AuthenticationManagerBuilder builder, UserDetailsService details, PasswordEncoder encoder) throws Exception {
-        builder.userDetailsService(details)
-                .passwordEncoder(encoder);
     }
 }
