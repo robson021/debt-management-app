1515413d057a925e030336efebcabb94315b13b6
rob <nowak021@gmail.com>
2018-06-29 21:06:01 +0200

    refactor mailer service. refactor app settings. update spring boot version

--- backend/src/main/java/robert/svc/MailerServiceImpl.java
+++ backend/src/main/java/robert/svc/MailerServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.util.Assert;
 import org.springframework.util.FileSystemUtils;
 import robert.svc.api.MailerService;
 
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.File;
 
@@ -34,25 +35,28 @@ public class MailerServiceImpl implements MailerService {
         Assert.hasText(receiverEmail, "Receiver cannot be null");
         asyncTaskExecutorService.submit(() -> {
             try {
-                MimeMessage mimeMessage = mailSender.createMimeMessage();
-                MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-                helper.setTo(receiverEmail);
-                helper.setSubject(topic);
-                helper.setText(body);
-                if (file != null) {
-                    FileSystemResource attachment = new FileSystemResource(file);
-                    helper.addAttachment(attachment.getFilename(), attachment);
-                }
-                mailSender.send(mimeMessage);
+                mailSender.send(createMessage(receiverEmail, topic, body, file));
                 log.info("Mail has been sent to {}", receiverEmail);
             } catch (Exception ex) {
                 log.error("Could not send email to {}\n{}", receiverEmail, ex.getMessage());
             } finally {
-                if (deleteFileAfterIsSent && file != null) {
+                if (deleteFileAfterIsSent && file != null)
                     FileSystemUtils.deleteRecursively(file);
-                }
             }
         });
+    }
+
+    private MimeMessage createMessage(String receiverEmail, String topic, String body, File file) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+        helper.setTo(receiverEmail);
+        helper.setSubject(topic);
+        helper.setText(body);
+        if (file != null) {
+            FileSystemResource attachment = new FileSystemResource(file);
+            helper.addAttachment(attachment.getFilename(), attachment);
+        }
+        return mimeMessage;
     }
 
 }
